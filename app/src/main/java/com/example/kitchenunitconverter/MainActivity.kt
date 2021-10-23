b38c8328f2da81be4d7888677d216a91a76b9245
SpencerDamon <spencerdamon1974@gmail.com>
2021-10-22 17:54:39 -0700

    Set click listener on convert button Get text from editable text and convert to float Checked radio button conversions added Conversion added Round value toggle set to round kotlin.math plugin

--- app/src/main/java/com/example/kitchenunitconverter/MainActivity.kt
+++ app/src/main/java/com/example/kitchenunitconverter/MainActivity.kt
@@ -12,5 +12,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.convertButton.setOnClickListener{ convertUnits() }
+    }
+
+    fun convertUnits () {
+        val stringInTextField = binding.amountToConvert.text.toString()
+        val amountToConvert = stringInTextField.toFloat()
+        val selectedUnitId = binding.conversionOptions.checkedRadioButtonId
+        val convertUnit = when (selectedUnitId) {
+            R.id.option_ml_to_fl_oz -> 0.033814
+            R.id.option_fl_oz_to_ml -> 29.574
+            R.id.option_grams_to_ounces -> 0.353
+            else -> 28.3495
+        }
+        var convertedAmount = convertUnit * amountToConvert
+
+        val round = binding.roundUpSwitch.isChecked
+        if (round) {
+            convertedAmount = kotlin.math.round(convertedAmount)
+        }
     }
 }
